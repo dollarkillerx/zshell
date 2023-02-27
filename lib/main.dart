109c4d894a506f3f8cfc12e73b06a9e3e8fd3ec3
dollarkillerx <adapawang@gmail.com>
2023-02-27 17:20:48 +0800

    Realization of basic functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'dart:convert';
-
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:desktop_multi_window/desktop_multi_window.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_pty/flutter_pty.dart';
 import 'package:get/get.dart';
-import 'package:xterm/xterm.dart';
 import 'package:zshell/pages/ssh_client/ssh_client.dart';
 
 import 'common/library/tools.dart';
@@ -90,89 +87,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // 不显示debug
       color: Colors.transparent,
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  Home({Key? key}) : super(key: key);
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  final terminal = Terminal(
-    maxLines: 10000,
-  );
-
-  final terminalController = TerminalController();
-
-  late final Pty pty;
-
-  @override
-  void initState() {
-    super.initState();
-
-    WidgetsBinding.instance.endOfFrame.then(
-      (_) {
-        if (mounted) _startPty();
-      },
-    );
-  }
-
-  void _startPty() {
-    pty = Pty.start(
-      shell,
-      columns: terminal.viewWidth,
-      rows: terminal.viewHeight,
-    );
-
-    pty.output
-        .cast<List<int>>()
-        .transform(Utf8Decoder())
-        .listen(terminal.write);
-
-    pty.exitCode.then((code) {
-      terminal.write('the process exited with exit code $code');
-    });
-
-    terminal.onOutput = (data) {
-      pty.write(const Utf8Encoder().convert(data));
-    };
-
-    terminal.onResize = (w, h, pw, ph) {
-      pty.resize(h, w);
-    };
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: SafeArea(
-        child: TerminalView(
-          terminal,
-          controller: terminalController,
-          autofocus: true,
-          backgroundOpacity: 0.7,
-          onSecondaryTapDown: (details, offset) async {
-            final selection = terminalController.selection;
-            if (selection != null) {
-              final text = terminal.buffer.getText(selection);
-              terminalController.clearSelection();
-              await Clipboard.setData(ClipboardData(text: text));
-            } else {
-              final data = await Clipboard.getData('text/plain');
-              final text = data?.text;
-              if (text != null) {
-                terminal.paste(text);
-              }
-            }
-          },
-        ),
-      ),
     );
   }
 }
